73258ebd074ce2475015c1e027b878f310125684
Felipe Sant Ana <35671903+FelipeSantanaIV@users.noreply.github.com>
2024-06-13 14:14:00 -0300

    Create TreeBinaryProblem.java, problema 3

---
+++ src/problemas/TreeBinaryProblem.java
@@ -0,0 +1,53 @@
+package problemas;
+
+import dataStructure.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Dada uma árvore binária, encontre o maior valor em cada nível.
+ *
+ * Solução:
+ * 1 - Utilize o algoritmo BFS(Busca em Largura) para percorrer a árvore e uma lista auxiliar.
+ * 2 - Para cada elemento e seu nível na árvore:
+ *  ° Se a lista possuir o índice == nível, substitua pelo maior valor
+ *  ° Senão adicione na lista
+ */
+
+public class TreeBinaryProblem {
+
+    public static List<Integer> largestValues(Tree tree) {
+        var list = new ArrayList<Integer>();
+        solveTree(tree.root, 0, list);
+        return list;
+    }
+
+    private static void solveTree(Tree.Node node,
+                                  int level,
+                                  ArrayList<Integer> list) {
+        if(node == null) return;
+        if (level == list.size()){
+            list.add(node.value);
+        } else {
+            list.set(level, Math.max(list.get(level), node.value));
+        }
+
+        solveTree(node.left, level+1, list);
+        solveTree(node.right, level+1, list);
+    }
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+
+        tree.insert(100);
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(6);
+        tree.insert(1);
+        tree.insert(7);
+        tree.insert(5);
+
+        System.out.println(largestValues(tree));
+    }
+}
